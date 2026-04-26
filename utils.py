04052e4b4812e4a06f12b3761322a057362b01f2
Lucaa220 <luca.crobu02@gmail.com>
2026-04-26 11:30:52 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@ from telegram.helpers import escape_markdown
 
 
 def safe_escape_markdown(value) -> str:
-    """Escape di sicurezza per MarkdownV2; non solleva eccezioni e gestisce None."""
     try:
         if value is None:
             return ""
